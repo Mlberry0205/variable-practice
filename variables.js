ab1ad2c8083c4290d00e5854706c6976beb70959
morgan berry <morganberry@morgans-MacBook-Pro.local>
2022-04-14 14:27:33 -0600

    added annotations

--- variables.js
+++ variables.js
@@ -1,11 +1,14 @@
+// Created 3 variables assigned to strings
 var redHair = "sassy"
 var animals = "dogs and cats"
 var weather = "cloudy with a chance of rain"
 
+// Created 3 variables assigned to integers
 var age = 31
 var birthday = 2591
 var year = 2022
 
+// Created 3 variables assigned to booleans
 var books = true
 var beach = false
 var table = false
